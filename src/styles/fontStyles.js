3dfedd9f5ec1dc906d0f51c65af95a5b68615fd9
Javi <javi.emiliano@gmail.com>
2025-05-31 02:27:54 -0300

    first commit

---
+++ src/styles/fontStyles.js
@@ -0,0 +1,25 @@
+const fontStyles = {
+    titleMain: {
+      fontFamily: '"Montserrat", sans-serif',
+      fontSize: '2.5rem',
+      fontWeight: 'bold',   
+    },
+    subtitleImportant: {
+      fontFamily: '"Roboto", sans-serif',
+      fontSize: '1.75rem', 
+      fontWeight: 500,    
+    },
+    headingItem: {
+      fontFamily: '"Raleway", sans-serif',
+      fontSize: '1.5rem', 
+      fontWeight: 600,  
+    },
+    bodyText: {
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '1rem',
+      lineHeight: 1.6, 
+    },
+  };
+  
+  export default fontStyles;
+  
